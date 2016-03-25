b2476a757069004f30ec6d87ac865d13fb3dcc21
vicky <lch82327@gmail.com>
2016-03-25 10:39:19 +0800

    update unit test for GF(256) arithmetic operation

---
+++ AES/GF256_spec.h
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include "GF256.hpp"
+
+// Test Constructor
+TEST(GF256, ADD) {
+
+  // EXPECT_EQ(GF256_mult(0xb6,0x53,0x1b), 0x36);
+  EXPECT_EQ(GF256_add(0x19,0x0d), 0x14);
+  EXPECT_EQ(GF256_add(0x53,0xca), 0x99);
+  /*
+      00011001
+      00001101
+      --------
+      00010100
+   */
+}
+
+TEST(GF256, MUL) {
+
+  EXPECT_EQ(GF256_mult(0x53,0xca,0x1b), 0x01);
+  EXPECT_EQ(GF256_mult(0xb6,0x53,0x1b), 0x36);
+
+  /*
+   76543210
+   01010011
+   11001010
+   --------
+   00010100
+   */
+}
