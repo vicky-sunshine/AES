fd8e7aa78baabd95bb027bfddb12061882f87990
vicky <lch82327@gmail.com>
2016-03-28 22:37:09 +0800

    remove attribute mx. no need since AES spec use 0x1b for modulo mx

--- AES/GF256.cpp
+++ AES/GF256.cpp
@@ -7,10 +7,10 @@ uint8_t GF256_add(uint8_t a, uint8_t b) {
 }
 
 // Multiplied by x. mx is the irreducible polynomial
-uint8_t GF256_mult_x(uint8_t a, uint8_t mx) {
+uint8_t GF256_mult_x(uint8_t a) {
   if (a & 0x80) {
     // highest bit == 1
-    return (a << 1) ^ mx ;
+    return (a << 1) ^ 0x1b ;
   } else {
     // highest bit == 0
     return a << 1;
@@ -18,14 +18,14 @@ uint8_t GF256_mult_x(uint8_t a, uint8_t mx) {
 }
 
 // General multiplication: mx is the irreducible polynomial
-uint8_t GF256_mult(uint8_t a, uint8_t b, uint8_t mx) {
+uint8_t GF256_mult(uint8_t a, uint8_t b) {
   uint8_t value = 0;
 
   for (int i = 0; i < 8; i++) {
     if (b & 0x01) {
       value = value ^ a;
     }
-    a = GF256_mult_x(a, mx);
+    a = GF256_mult_x(a);
     b = b >> 1;
   }
 
--- AES/GF256.hpp
+++ AES/GF256.hpp
@@ -12,13 +12,13 @@ uint8_t GF256_add(uint8_t a, uint8_t b);
 Multiplied by x in GF(2^8).
 mx is the irreducible polynomial
 */
-uint8_t GF256_mult_x(uint8_t a, uint8_t mx);
+uint8_t GF256_mult_x(uint8_t a);
 
 /*
 General Multiplication in GF(2^8)
 mx is the irreducible polynomial
 */
-uint8_t GF256_mult(uint8_t a, uint8_t b, uint8_t mx);
+uint8_t GF256_mult(uint8_t a, uint8_t b);
 
 
 uint8_t GF256_inv(uint8_t *a, uint8_t mx);
