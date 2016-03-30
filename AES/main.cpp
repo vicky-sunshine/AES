1612790008ff9c63c5029ddcf2a45f386e61fe0a
vicky <lch82327@gmail.com>
2016-03-30 13:06:33 +0800

    add AES decryotion test

--- AES/main.cpp
+++ AES/main.cpp
@@ -13,27 +13,34 @@ int main(int argc, char ** argv) {
     0xed, 0xcf, 0x76, 0xa6,
     0x08, 0xa3, 0xb6, 0x78,
     0x31, 0x31, 0x27, 0x6e};
+  uint8_t round_key[176];
+  
   uint8_t plaintext[16] = {
     0xa3, 0xc5, 0x08, 0x08,
     0x78, 0xa4, 0xff, 0xd3,
     0x00, 0xff, 0x36, 0x36,
     0x28, 0x5f, 0x01, 0x02};
-
   uint8_t ciphertext[16];
-  uint8_t round_key[176];
+  uint8_t result[16];
 
   key_expansion(key, round_key);
   AES_Encrypt(plaintext, ciphertext, round_key);
 
-  printf("\nin:\n");
+  printf("\nplaintext:\n");
   for (int i = 0; i < 4; i++) {
     printf("%2x %2x %2x %2x\n", plaintext[4*i+0], plaintext[4*i+1], plaintext[4*i+2], plaintext[4*i+3]);
   }
 
-  printf("\nout:\n");
+  printf("\nciphertext:\n");
   for (int i = 0; i < 4; i++) {
     printf("%2x %2x %2x %2x\n", ciphertext[4*i+0], ciphertext[4*i+1], ciphertext[4*i+2], ciphertext[4*i+3]);
   }
+  AES_Decrypt(result, ciphertext, round_key);
+  printf("\nafter decrypt:\n");
+  for (int i = 0; i < 4; i++) {
+    printf("%2x %2x %2x %2x\n", result[4*i+0], result[4*i+1], result[4*i+2], result[4*i+3]);
+  }
+
 
 
   return 0;
